d2fba47277978b92c0ebab3e47f5c68adf4a19b7
huseyintopuz <huseyin.topuz35@gmail.com>
2022-07-09 22:57:54 +0300

    insurance

--- src/components/Inform.js
+++ src/components/Inform.js
@@ -1,5 +1,6 @@
+import Spinner2 from './Spinner2';
 import { useSelector } from 'react-redux';
-import './styles/inform.scss'
+import './styles/inform.scss';
 
 const Inform = () => {
     const { offerCount, getOffers, products, isLoading3, isLoading4 } = useSelector(state => state.products);
@@ -11,36 +12,40 @@ const Inform = () => {
         <>
             {!isLoading3 &&
                 <div className='inform'>
-                    <button type="button" class="btn btn-primary " data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    <button type="button" className="btn btn-primary " data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                         There are {offerCount} insurance offers for you.
                     </button>
 
-                    {!isLoading4 && (offersLength === offerCount) &&
-                        <div class="modal fade" id="staticBackdrop" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-scrollable">
-                                <div class="modal-content">
-                                    <div class="modal-body">
+                    {isLoading4 && (offersLength !== offerCount) ?
+                        <Spinner2 /> :
+                        !isLoading4 && (offersLength === offerCount) &&
+                        <div className="modal fade" id="staticBackdrop" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div className="modal-dialog modal-dialog-scrollable">
+                                <div className="modal-content">
+                                    <div className="modal-body">
                                         <ul className='product-group'>
-                                            {getOffers && getOffers.map((offer, index) => {
-                                                return (
-                                                    <li key={index} className='product'>
-                                                        <div>
-                                                            <img width='160px' height='50px' src={offer.ImagePath} alt='insurance logo' />
-                                                        </div>
-                                                        <div className='info'>
-                                                            <div className='title'>
-                                                                <span>{offer.ProductDesc}</span>
-                                                                <span>{offer.FirmName}</span>
+                                            {getOffers
+                                                .slice().sort((a, b) => a.Cash - b.Cash)
+                                                .map((offer, index) => {
+                                                    return (
+                                                        <li key={index} className='product'>
+                                                            <div>
+                                                                <img width='160px' height='50px' src={offer.ImagePath} alt='insurance logo' />
                                                             </div>
-                                                            <div className='price'>
-                                                                <div>{offer.QuotaInfo.HasDiscount === true ? 'Peşin' : ''} <span className={(offer.QuotaInfo.HasDiscount === true) ? 'cash' : 'bold'}>{offer.Cash} TL</span> </div>
-                                                                {offer.QuotaInfo.HasDiscount === true && <h4>{offer.QuotaInfo.PremiumWithDiscount} TL</h4>}
-                                                                <button className={offer.QuotaInfo.HasDiscount === true ? 'btn btn-primary' : 'btn btn-outline-primary'} type="button">{(offer.QuotaInfo.HasDiscount === true) ? 'SATIN AL' : 'TELEFONDA SATIN AL'}</button>
+                                                            <div className='info'>
+                                                                <div className='title'>
+                                                                    <span>{offer.ProductDesc}</span>
+                                                                    <span>{offer.FirmName}</span>
+                                                                </div>
+                                                                <div className='price'>
+                                                                    <div>{offer.QuotaInfo.HasDiscount === true ? 'Peşin' : ''} <span className={(offer.QuotaInfo.HasDiscount === true) ? 'cash' : 'bold'}>{Math.round(offer.Cash)} TL</span> </div>
+                                                                    {offer.QuotaInfo.HasDiscount === true && <h4>{Math.round(offer.QuotaInfo.PremiumWithDiscount)} TL</h4>}
+                                                                    <button className={offer.QuotaInfo.HasDiscount === true ? 'btn btn-primary' : 'btn btn-outline-primary'} type="button">{(offer.QuotaInfo.HasDiscount === true) ? 'SATIN AL' : 'TELEFONDA SATIN AL'}</button>
+                                                                </div>
                                                             </div>
-                                                        </div>
-                                                    </li>
-                                                )
-                                            })}
+                                                        </li>
+                                                    )
+                                                })}
                                         </ul>
                                     </div>
                                 </div>
--- src/components/Product.js
+++ src/components/Product.js
@@ -19,8 +19,8 @@ const Product = () => {
                                     <span>{product.FirmName}</span>
                                 </div>
                                 <div className='price'>
-                                    <div>{product.QuotaInfo.HasDiscount === true ? 'Peşin' : ''} <span className={(product.QuotaInfo.HasDiscount === true) ? 'cash' : 'bold'}>{product.Cash} TL</span> </div>
-                                    {product.QuotaInfo.HasDiscount === true && <h4>{product.QuotaInfo.PremiumWithDiscount} TL</h4>}
+                                    <div>{product.QuotaInfo.HasDiscount === true ? 'Peşin' : ''} <span className={(product.QuotaInfo.HasDiscount === true) ? 'cash' : 'bold'}>{Math.round(product.Cash)} TL</span> </div>
+                                    {product.QuotaInfo.HasDiscount === true && <h4>{Math.round(product.QuotaInfo.PremiumWithDiscount)} TL</h4>}
                                     <button className={product.QuotaInfo.HasDiscount === true ? 'btn btn-primary' : 'btn btn-outline-primary'} type="button">{(product.QuotaInfo.HasDiscount === true) ? 'SATIN AL' : 'TELEFONDA SATIN AL'}</button>
                                 </div>
                             </div>
